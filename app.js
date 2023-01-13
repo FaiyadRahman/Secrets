4b7d7a3768e4ff5ffdca4598b200a14b6bd2d06c
FaiyadRahman <faiyad.r@yahoo.com>
2023-01-12 21:52:27 -0700

    add password encryption

--- app.js
+++ app.js
@@ -1,46 +1,70 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
 app.use(express.static("public"));
-app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.set('strictQuery', true);
+mongoose.set("strictQuery", true);
 mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 
-const userSchema = {
-    email: String,
-    password: String
-};
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+userSchema.plugin(encrypt, {
+  secret: process.env.SECRET,
+  encryptedFields: ["password"],
+});
 
 const User = new mongoose.model("User", userSchema);
 
 app.listen(3000, () => {
-    console.log("Server listening on port 3000")
+  console.log("Server listening on port 3000");
 });
 
 app.get("/", (req, res) => {
-    res.render("home");
+  res.render("home");
 });
 
 app.get("/login", (req, res) => {
-    res.render("login");
+  res.render("login");
 });
 
 app.get("/register", (req, res) => {
-    res.render("register");
+  res.render("register");
 });
 
 app.post("/register", (req, res) => {
-    const email = req.body.username;
-    const password = req.body.password;
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
 
-    const newUser = new User({
-        email: email,
-        password: password
-    });
-})
+  newUser.save((err) => {
+    if (!err) {
+      res.render("secrets");
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+app.post("/login", (req, res) => {
+  User.findOne({ email: req.body.username }, (err, user) => {
+    if (!err) {
+      if (user.password === req.body.password) {
+        res.render("secrets");
+      }
+    } else {
+      console.log(err);
+    }
+  });
+});
