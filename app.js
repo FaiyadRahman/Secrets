2873a65d2f20b91e8c9fbf799b1f8fdec6c8ed6b
FaiyadRahman <faiyad.r@yahoo.com>
2023-01-12 21:13:27 -0700

    register usernames and password

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const { appendFile } = require("fs");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -9,6 +9,38 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended:true}));
 
+mongoose.set('strictQuery', true);
+mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
 app.listen(3000, () => {
     console.log("Server listening on port 3000")
+});
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+app.post("/register", (req, res) => {
+    const email = req.body.username;
+    const password = req.body.password;
+
+    const newUser = new User({
+        email: email,
+        password: password
+    });
 })
